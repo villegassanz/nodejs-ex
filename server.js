bd0d0f9d4e6f53e2a3734c817c6235be52a36364
villegas <villegas09saz@gmail.com>
2016-12-10 21:37:19 -0600

    version dos

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').Server(app);  
+
+
+
+app.use(express.static('public'));
+
+app.get('/', function(request,response){
+  response.sendFile(__dirname + '/index.html');
+});
+
+
+server.listen(8080, function() {  
+  console.log("Servidor corriendo en http://localhost:8080");
+});
