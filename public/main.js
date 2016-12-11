d0cce9f6519430c7879a08d91909df6fafe65382
villegas <villegas09saz@gmail.com>
2016-12-10 21:50:09 -0600

    actualizacion tres

---
+++ public/main.js
@@ -0,0 +1,35 @@
+var socket = io.connect('http://nodejs1-proyectov9.44fs.preview.openshiftapps.com/:8080', { 'forceNew': true });
+
+socket.on('messages', function(data) {  
+  console.log(data);
+  render(data);
+})
+//agregar los datos al html
+function render (data) {  
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <strong>${elem.id_autobus}</strong>:
+              <strong>${elem.rfc}</strong>
+			  <strong>${elem.ruta}</strong>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {  
+  var message = {
+    id_autobus: document.getElementById('id_autobus').value,
+    rfc: document.getElementById('rfc').value,
+	ruta: document.getElementById('ruta').value,
+	eje:document.getElementById('ejemplo').value,
+	//lat:document.getElementById('latitud').value,
+	//lon:doc.getElementById('longitud').value,
+	//fecha:doc.getElementById('fecha').value,
+	//horai:doc.getElementById('hora inicio').value,
+	//horaf:doc.getElementById('hora fin').value
+  };
+
+  socket.emit('new-message', message);
+  return false;
+}
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');  
 var app = express();  
-var server = require('http').Server(app);  
+var server = require('http').Server(app);
+var io = require('socket.io')(server); 
 
+var messages = [
 
+];
 
 app.use(express.static('public'));
 
@@ -10,7 +13,20 @@ app.get('/', function(request,response){
   response.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket) {  
+  console.log('Alguien se ha conectado con Sockets');
+  
+	socket.emit('messages', messages);
+ 
+  socket.on('new-message', function(data) {
+    messages.push(data);
+    io.sockets.emit('messages', messages);
+
+    
+  });
+
+});
 
 server.listen(8080, function() {  
-  console.log("Servidor corriendo en http://localhost:8080");
+  console.log("Servidor corriendo en http://nodejs1-proyectov9.44fs.preview.openshiftapps.com/:8080");
 });
