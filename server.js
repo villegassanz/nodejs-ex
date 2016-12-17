e09d33d8dcacc79918122419326ca21d9b40713f
villegas <villegas09saz@gmail.com>
2016-12-17 08:07:23 -0600

    Actualizacion bunx

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ var express = require('express');
 var app = express();  
 var server = require('http').Server(app);
 var io = require('socket.io')(server); 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-
-
-var db = mongoose.connect('mongodb://villegas:12345@mongodb/buslocation');
-mongoose.connection.once('connected', function() {
-	console.log("Connected to database")
-});
-
 
 var puerto = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
@@ -19,20 +9,6 @@ var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var messages = [
 
 ];
-var Modelo_Mensaje = mongoose.model('principal', MensajeSchema);
-
-var MensajeSchema = mongoose.Schema({
-    _id: String,
-    rfc: String,
-    ruta: String,
-    ejemplo:String,
-    latitud: String,
-    longitud: String,
-    fecha: Date,
-    hora_inicio: Date,
-    hora_fin: Date
-});
-
 
 app.use(express.static('public'));
 
@@ -53,13 +29,6 @@ io.on('connection', function(socket) {
   });
 
 });
- var nuevoMensaje = new Modelo_Mensaje({
-          _id: data.id_autobus, //en esta parte recibimos del arreglo data los valores que ingreso el usuario
-          rfc: data.rfc,
-          ruta: data.ruta,
-          ejemplo: data.eje,
-          fecha: new Date()
-      });
 
 server.listen(8080, function() {  
   console.log("Servidor listo en el puerto");
