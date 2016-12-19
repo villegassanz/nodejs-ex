18f54c47a5552f5506c764abc5a2381de7e28259
villegassanz <villegas09sanz@gmail.com>
2016-12-18 19:29:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,52 @@
-var express = require('express');  
-var app = express();  
-var server = require('http').Server(app);
-var io = require('socket.io')(server); 
+/*
+ * Primero necesitamos crear nuestro
+ * servidor que será un servidor http
+ */
+var http = require('http');
+var mongoose = require('mongoose'); // Definimos el modulo de mongoose
 
-var puerto = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+// Conectamos con la base de datos
+mongoose.connect('mongodb://admin:zIfrXnRChzVx@127.7.213.130:27017/buslocation');  
 
-var messages = [
+//mongoose.connect('mongodb://localhost/users');
 
-];
+// Definimos el Schema de la base de datos
+Users = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  surname: String
+}, { collection : 'usersList'}); // Añadimos la colleción que vamos acceder
 
-app.use(express.static('public'));
 
-app.get('/', function(request,response){
-  response.sendFile(__dirname + '/index.html');
-});
+// Añadimos el schema al modelo
+User = mongoose.model('users', Users);
 
-io.on('connection', function(socket) {  
-  console.log('Alguien se ha conectado con Sockets');
-  
-	socket.emit('messages', messages);
- 
-  socket.on('new-message', function(data) {
-    messages.push(data);
-    io.sockets.emit('messages', messages);
+/*
+ * Creamos el servidor y un callback
+ * que contendrá una respuesta(res) y una
+ * solicitud(req)
+ */
+function peticionServidor(req,res){
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+    // Buscamos todos los usuarios
+    User.find(function (err, data) {
+      if (err) { return console.error(err); } // Si hay un error
+      if (data === null) { // Si no hay ningún usuario
+        console.log('There aren\'t users in database');
+      }
+    // data será todos los usuarios encontrados y lo que nos devuelve
+    // será un array con todos los datos, esto se puede hacer de 
+    // una manera mucho más sencilla que veremos más adelante:
+    res.write('id\tname\tsurname\n'
+      + data[0]._id + '\t' + data[0].name + '\t' + data[0].surname + '\n'
+      + data[1]._id + '\t' + data[1].name + '\t' + data[1].surname);
+    res.end();
+    });
+}
 
-    
-  });
 
-});
 
-server.listen(8080, function() {  
-  console.log("Servidor listo en el puerto");
-});
-
+http.createServer(peticionServidor).listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+}); // Escuchamos el servidor por el puerto 3000
+Contact GitHub 
