ecde4b538ae06708f734666334233b75f04ee585
villegas <villegas09saz@gmail.com>
2016-12-19 17:43:49 -0600

    actualizando vv00

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');  
 var app = express();  
+var http = require('http').Server(app);
 var server = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
