dd51b5a2a3b1d0d3361a23f8e304cc87458406a4
villegas <villegas09saz@gmail.com>
2016-12-19 19:06:58 -0600

    actualizando vv0011

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');  
 var app = express();  
+//var http = require('http').Server(app);
 var http = require('http').Server(app);
-//var server = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 
